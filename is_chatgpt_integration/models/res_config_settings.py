24a1449db5e7fd9b8d669d1d8b4826ab2b76f2a9
ChristopheVdd <167391537+C-VERP@users.noreply.github.com>
2025-06-10 22:55:00 +0200

    Update res_config_settings.py

--- is_chatgpt_integration/models/res_config_settings.py
+++ is_chatgpt_integration/models/res_config_settings.py
@@ -8,7 +8,7 @@ class ResConfigSettings(models.TransientModel):
     _inherit = "res.config.settings"
 
     def _get_default_chatgpt_model(self):
-        return none
+        return
 
     openapi_api_key = fields.Char(string="API Key", help="Provide the API key here", config_parameter="is_chatgpt_integration.openapi_api_key")
     chatgpt_model_id = fields.Many2one('chatgpt.model', 'ChatGPT Model', ondelete='cascade', default=_get_default_chatgpt_model,  config_parameter="is_chatgpt_integration.chatgp_model")
